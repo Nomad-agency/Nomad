5942f645a560f760978fd0e21fa294232fd3ae9e
lunglinhlaplanh <158856626+hungdev1602@users.noreply.github.com>
2025-02-21 17:36:03 +0300

    Send Mail

--- src/pages/HomePage/sections/Section5/components/UI/FormSection/FormSection.jsx
+++ src/pages/HomePage/sections/Section5/components/UI/FormSection/FormSection.jsx
@@ -1,13 +1,69 @@
-import React from 'react';
+import React, { useState } from 'react';
 import InputSection from '../InputSection/InputSection';
-
+import emailjs from '@emailjs/browser';
 const FormSection = () => {
+  const [name, setName] = useState("");
+  const [project, setProject] = useState("");
+  const [mail, setMail] = useState("");
+  const [budget, setBudget] = useState("");
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    
+    // EmailJS service ID, template ID, and public key
+    const serviceId = 'service_9z33cas';
+    const templateId = 'template_t9ewhcd';
+    const publicKey = 'XCJKd_CXhpWlFmiqH';
+
+    // object that contains dynamic template params
+    const templateParams = {
+      from_name: name,
+      project: project,
+      mail: mail,
+      budget: budget,
+    };
+    // send email
+    emailjs.send(serviceId, templateId, templateParams, publicKey)
+      .then((response) => {
+        setName("");
+        setProject("");
+        setMail("");
+        setBudget("");
+        alert("Письмо успешно отправлено");
+      })
+      .catch((error) => {
+        console.error('Email sending failed:', error);
+      });
+  }
   return (
-    <form className="grid grid-cols-2 lg:grid-cols-5 gap-7 lg:gap-20 xl:gap-[100px] mt-10 sm:mt-12">
-      <InputSection type="text" placeholder="Имя" />
-      <InputSection type="text" placeholder="Проект" />
-      <InputSection type="email" placeholder="Почта" />
-      <InputSection type="text" placeholder="Бюджет" />
+    <form 
+      className="grid grid-cols-2 lg:grid-cols-5 gap-7 lg:gap-20 xl:gap-[100px] mt-10 sm:mt-12"
+      onSubmit={handleSubmit}
+    >
+      <InputSection 
+        type="text" 
+        placeholder="Имя" 
+        value={name}
+        setValue={setName}
+      />
+      <InputSection 
+        type="text" 
+        placeholder="Проект" 
+        value={project}
+        setValue={setProject}
+      />
+      <InputSection 
+        type="email"
+        placeholder="Почта" 
+        value={mail}
+        setValue={setMail}
+      />
+      <InputSection 
+        type="text" 
+        placeholder="Бюджет" 
+        value={budget}
+        setValue={setBudget}
+      />
 
       <button type="submit" className="bg-secondary text-white py-[10px] rounded-[15px] col-span-2 lg:col-span-1">
         Отправить
--- src/pages/HomePage/sections/Section5/components/UI/InputSection/InputSection.jsx
+++ src/pages/HomePage/sections/Section5/components/UI/InputSection/InputSection.jsx
@@ -3,7 +3,7 @@ import styles from './index.module.css';
 import gsap from 'gsap';
 import { useGSAP } from '@gsap/react';
 
-const InputSection = ({ type = 'text', placeholder = '' }) => {
+const InputSection = ({ type = 'text', placeholder = '', value, setValue }) => {
   gsap.registerPlugin(useGSAP);
   const container = useRef(null);
   const boxRef = useRef(null);
@@ -19,12 +19,14 @@ const InputSection = ({ type = 'text', placeholder = '' }) => {
   return (
     <div ref={container} className={'w-auto pb-[10px] p-[2px]'} style={{ display: 'grid' }}>
       <input
+        value={value}
         type={type}
         className="outline-none text-sm border-b-2"
         placeholder={placeholder}
         required
         onFocus={() => onFocus(true)}
         onBlur={() => onFocus(false)}
+        onChange={(e) => setValue(e.target.value)}
       />
       <div ref={boxRef} className={styles.className}></div>
     </div>
